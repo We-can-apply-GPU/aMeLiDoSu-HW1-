4e741120cf2ab9e698b5da2ccc1109201b992777
TonyHsu <b02901085@ntu.edu.tw>
2015-03-26 00:30:11 +0800

    conflict solved

--- dnn/network.py
+++ dnn/network.py
@@ -5,66 +5,87 @@ File: network.py
 Description: define the whole dnn,and learning alg 
 """
 import numpy as np
-from util import *
 import layer
+from util import *
 
 
 class Network:
     def __init__(self,sizes):
         self._numLayers = len(sizes)
         self._sizes = sizes
-        self._weights = [np.random.randn(j,i)
-                        for(i,j) in zip(sizes[:-1],sizes[1:])]
-        self._biases =[np.random.randn(b,1)for b in sizes[1:]]
-        self._deltaW = [np.zeros(j,i) 
-                for (i,j) in zip(sizes[:-1],[sizes[1:]])]
-        self._deltaB = [np.zeros(b,1) for b in sizes[1:]]
+            
+        self._gradW= [np.zeros(j,i) \
+                    for (i,j) in zip(sizes[:-1],[sizes[1:]])]
+        self._gradB = [np.zeros(b,1)\
+                    for b in sizes[1:]]
         self._layers = []
 
-        for i in xrange(self._numLayers)
-            layer = Layer(sizes[i])
+        for size in sizes:
+            layer = Layer(size)
             self._layers.append(layer)
+######################
+    def initialize(self,parsPath=""):
+        if(parsPath ==""):
+            self._weights = [np.random.randn(j,i)\
+                    for(i,j) in zip(sizes[:-1],sizes[1:])]
+            self._biases =[np.random.randn(b,1)\
+                    for b in sizes[1:]]
+        else:
+            #self.loadModel(parsPath)
+            pass
+######################
+    def train(self,batch):
+        for data in batch:
+            dnn.forward(data)
+            dnn.errorFunc()
+            dnn.backpro()
+            dnn.update()
+###################
+    def forward(self,inData):
+        #z is input  vector of neuron layer
+        #a is output vector of neuron layer,a=activate(z)
 
-        #load the test data to the first layer 'z'
-
-
-
-
-    def forward(self,inData) #z is input vector of neuron layer
         #inData must be a np.array
         self._layers[0]._z = inData
         for b,w,z,a in zip(self._biases,self._weights,
                          self._layers[1:],self._layers[:-1]):
-            z = activate(np.dot(w,a) + b)#dot is matrix product
+            z = activate(np.dot(w,a) + b)
+            #dot can used on matrix product
             #the last layer input z is the output of dnn
-        return z[-1]
+        return (z[-1]/np.linalg.norm(z[-1]))#normalize it!
+#############################
 
     def backpro(self):
-        deltaW = 2 * np.dot(sigmoidprime(self._layers[-1]._z)
+        deltaW[-1] = 2 * \
+            np.dot(sigmoidprime(self._layers[-1]._z))
 
-        for l in xrange(2,len(self._numLayers)-1)
-            self.deltaW[-l] = 
-                np.dot(sigmoidprime(self._layers[-l]._z),
-                    np.dot(np.transpose(self.weights[-l+1]),
-                        deltaW[-l+1]))              
-            self.deltaB[-l] = 
-                np.dot(sigmoidprime(self._layers[-l]._z),
-                    np.dot(np.transpose(self.weights[-l+1]),
+        for l in xrange(2,self._numLayers-1):
+            self._gradW[-l] = np.dot\
+                    (sigmoidprime(self._layers[-l]._z),\
+                    np.dot(np.transpose(self.weights[-l+1]),\
+                           deltaW[-l+1]))              
+            self._gradB[-l] = np.dot\
+                    (sigmoidprime(self._layers[-l]._z),\
+                    np.dot(np.transpose(self.weights[-l+1]),\
                         deltaB[-l+1]))              
+######################
 
-    def update(self,zeta):
-        for w,i,b in zip(_weights,xrange(len(deltaW))):
-            w = np.subtract(w,deltaW[i]* zeta)
-            b = np.subtract(b,deltaB[i]* zeta)
+    def update(self,eta):
+        for w,b,i in zip(self._weights,self._biases,
+                xrange(len(self._numLayers)-1)):
+            w = np.subtract(w,gradW[i]* eta)
+            b = np.subtract(b,gradB[i]* eta)
 
 
     def predict(self,inData):
         conseq = self.forward(inData)
 
         #Mapping to 39 phome
-
+        pass
 
 
     def loadModel(self):
+        pass
 
     def saveModel(self):
+        pass
---
+++ dnn/util/errorFunc.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: errorFunc.py
+Description: evaluate the cost function
+"""
+
