902b795c82002ba3c38fd86a9771bba922fca31b
b02901088 <b02901088@linux8.csie.ntu.edu.tw>
2015-03-27 17:34:46 +0800

    fix bug

--- dnn/util/errorFunc.py
+++ dnn/util/errorFunc.py
@@ -10,41 +10,51 @@ def errFunc(ls):
     return errSquare(ls)
 def errFuncGradient(ls):
     r = []
-    newls = []
-    mart = f48t39()
     for i in ls:
-        newvec = mart*i[0]
-        newls.append((newvec,i[1]))
-    for i in newls:
         r.append(2*np.sum(i[0]-i[1]))
-    return r/ls.__len__()
+    return np.asarray(r/ls.__len__())
 def errSquare(ls):
     r = 0.0
-    newls = []
-    mart = f48t39()
     for i in ls:
-        newvec = mart*i[0]
-        newls.append((newvec,i[1]))
-    for i in newls:
         r += np.sum((i[0]-i[1])**2)
     return r/ls.__len__()
-def f48t39():
+
+#not completed
+def f48t39_2(vec):
     m = open("../../phones/48_39.map")
     mapdic = {}
     iter39 = {}
     cnt = 0
-    mart = []
+    ls39 = []
+    martls = []
     for s in m:
         tmp = s.rstrip().split("\t")
         mapdic[tmp[0]]=tmp[1]
         if tmp[1] not in iter39:
+            ls39.append(tmp[1]) 
             iter39[tmp[1]] = cnt
             cnt += 1
             vec = [0]*39
             vec[iter39[tmp[1]]] = 1
-            mart.append(vec)
-    return np.asarray(mart).transpose()
+            martls.append(vec)
+    mart = np.asarray(martls).transpose()
+    vec = mart*np.transpose(np.asarray(vec))
+    print(vec.shape)
+    return -1#ls39[np.argmax(vec)]
 
+def f48t39_1(vec):
+    m = open("../../phones/48_39.map")
+    mapdic = {}
+    ls48 = []
+    for s in m:
+        tmp = s.rstrip().split("\t")
+        mapdic[tmp[0]]=tmp[1]
+        ls48.append(tmp[0]) 
+    return mapdic[ls48[np.argmax(vec)]]
+arr = [0]*48
+arr[45] = 100
+arr = np.asarray(arr)
+print(f48t39_2(arr))
 #The errorFunc will receive one argument, a list,
 #and compare it with the valuse set in labels(need mapping)
 #errorFuncPrime will,too
