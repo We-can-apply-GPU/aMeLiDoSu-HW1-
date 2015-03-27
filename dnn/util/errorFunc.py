564a669f218af1dde44bd5b0bb7c3d96590ba423
b02901088 <b02901088@linux8.csie.ntu.edu.tw>
2015-03-27 16:09:33 +0800

    add gradient

--- dnn/util/errorFunc.py
+++ dnn/util/errorFunc.py
@@ -8,19 +8,42 @@ import numpy as np
 # write in the Network class??
 def errFunc(ls):
     return errSquare(ls)
+def errFuncGradient(ls):
+    r = []
+    newls = []
+    mart = f48t39()
+    for i in ls:
+        newvec = mart*i[0]
+        newls.append((newvec,i[1]))
+    for i in newls:
+        r.append(2*np.sum(i[0]-i[1]))
+    return r/ls.__len__()
 def errSquare(ls):
     r = 0.0
-    mapdic = {}
     newls = []
-    m = open("48_39.map")
+    mart = f48t39()
+    for i in ls:
+        newvec = mart*i[0]
+        newls.append((newvec,i[1]))
+    for i in newls:
+        r += np.sum((i[0]-i[1])**2)
+    return r/ls.__len__()
+def f48t39():
+    m = open("../../phones/48_39.map")
+    mapdic = {}
+    iter39 = {}
+    cnt = 0
+    mart = []
     for s in m:
         tmp = s.rstrip().split("\t")
         mapdic[tmp[0]]=tmp[1]
-    for i in ls:
-        newls.append((mapdic[i[0]],i[1]))
-    for i in newls:
-        r += np.sum((np.asarray(i[0])-np.asarray(i[1]))**2)
-    return r/ls.__len__()
+        if tmp[1] not in iter39:
+            iter39[tmp[1]] = cnt
+            cnt += 1
+            vec = [0]*39
+            vec[iter39[tmp[1]]] = 1
+            mart.append(vec)
+    return np.asarray(mart).transpose()
 
 #The errorFunc will receive one argument, a list,
 #and compare it with the valuse set in labels(need mapping)
