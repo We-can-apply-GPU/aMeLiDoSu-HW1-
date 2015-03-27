74b57f2b67ed4b404b685de4bfc49eb84a814da1
b02901088 <b02901088@linux5.csie.ntu.edu.tw>
2015-03-27 15:18:09 +0800

    err+map

--- dnn/util/errorFunc.py
+++ dnn/util/errorFunc.py
@@ -4,7 +4,23 @@
 File: errorFunc.py
 Description: evaluate the cost function
 """
+import numpy as np
 # write in the Network class??
+def errFunc(ls):
+    return errSquare(ls)
+def errSquare(ls):
+    r = 0.0
+    mapdic = {}
+    newls = []
+    m = open("48_39.map")
+    for s in m:
+        tmp = s.rstrip().split("\t")
+        mapdic[tmp[0]]=tmp[1]
+    for i in ls:
+        newls.append((mapdic[i[0]],i[1]))
+    for i in newls:
+        r += np.sum((np.asarray(i[0])-np.asarray(i[1]))**2)
+    return r/ls.__len__()
 
 #The errorFunc will receive one argument, a list,
 #and compare it with the valuse set in labels(need mapping)
