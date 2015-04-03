5a67672cf43733163687d51d886f6affefaf251a
Daniel Chin <danielthank@gmail.com>
2015-04-03 17:20:32 +0800

    Add evaluate function

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ import random
 #Predefined const
 sizes = [39,128,48]
 EPOCH_MAX = 1
-BATCH_SIZE = 3
+BATCH_SIZE = 100
 
 def main():
     dnn = Network()
@@ -25,26 +25,16 @@ def main():
     else:
         dnn.initialize(BATCH_SIZE,parsPath = "model/" + sys.argv[1])
     
-    dataset = iofile.infile("data/mfcc/trainToy.ark", "data/label/trainToy.lab")
+    dataset = iofile.infile("data/mfcc/train.ark", "data/label/train.lab")
     random.seed()
     random.shuffle(dataset)
     [trainData, trainLabel] = util.genBatchs(BATCH_SIZE, dataset)
     lenBatch = len(trainData)
-    #print(lenBatch)
     
     for z in range(EPOCH_MAX):
         for i in range(lenBatch):
-            dnn.train(np.transpose(np.array(trainData[i])), np.transpose(np.array(trainLabel[i])))
-
-    #training stage
-    #for i in range(EPOCH_MAX):
-        #print("{0}/{1}".format(i+1, EPOCH_MAX))
-        ##print(len(batchs))
-        #for batch in batchs:
-            #labels=[batch[i][1] for i in range(len(batch))]
-            #dnn.setLabel(labels)
-            #dnn.train(batch)
-        #dnn.reportErrorrate()
+            print("{0}/{1}".format(i, lenBatch))
+            dnn.train(np.transpose(np.array(trainData[i], dtype="float32")), np.transpose(np.array(trainLabel[i], dtype="float32")))
 
     if len(sys.argv) > 2:
         modelName = "model/" + str(sys.argv[2])
@@ -53,17 +43,26 @@ def main():
         modelName = "model/" + str(time.time())
     dnn.saveModel(modelName)
     
-    #out = open(modelName, "a")
-    #trans = []
-    #util.loadMapList(trans)
-    #cnt = 0
-    #for row in dataset:
-        #max_index = util.chooseMax(dnn.forward(row[0]))
-        #print(trans[max_index][1], row[1])
-        #if trans[max_index][1] == row[1]:
-            #cnt += 1
-    #print("{0}/{1} = {2}".format(cnt, len(dataset), float(cnt)/len(dataset)))
-    #out.write("{2}({1})".format(cnt, len(dataset), float(cnt)/len(dataset)))
+    out = open(modelName, "a")
+    trans = []
+    util.loadMapList(trans)
+    cnt = 0
+    allDatas = []
+    allLabels = []
+    for batch in trainData:
+        allDatas.extend(batch) 
+    for batch in trainLabel:
+        allLabels.extend(batch)
+
+    allOutputs= dnn.forward(np.transpose(np.array(allDatas, dtype="float32")))
+    allOutputs = allOutputs.T
+
+    for i in range(len(allOutputs)):
+        max_index = util.chooseMax(allOutputs[i])
+        if allLabels[i][max_index] == 1:
+            cnt += 1
+    print("{0}/{1} = {2}".format(cnt, len(allOutputs), float(cnt)/len(allOutputs)))
+    out.write("{2}({1})".format(cnt, len(allOutputs), float(cnt)/len(allOutputs)))
 
 if __name__ == "__main__":
     main()
