2f5a97fb2ca001e3d9b9c86593cac4e2e56f2f73
TonyHsu <sunprince12014@gmail.com>
2015-04-03 19:22:29 +0800

    predict modified

--- predict.py
+++ predict.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """
 File: predict.py
@@ -7,7 +6,7 @@ Desctiption:
 
 import sys
 from DNN.network import *
-from DNN.iofile import *
+from iofile import *
 import util
 
 
@@ -16,14 +15,27 @@ def main():
     dnn.initialize(1,parsPath = "model/" + sys.argv[1]) # batchsize is 1
     data = arkIn("data/mfcc/test.ark")
     out = open("output/" + sys.argv[1] + ".csv", "w")
+
     trans = []
-    util.loadMap(trans)
+    util.loadMapList(trans)
     cnt = 1
+    out.write("Id,Prediction\n")
+    inputData = []
     for row in data:
-        max_index = util.chooseMax(dnn.forward(row[1:]))
-        out.write("{0},{1}\n".format(row[0], trans[max_index][1]))
-        print("{0}/{1}".format(cnt, len(data)))
-        cnt += 1
+        inputData.append(row[1:])
+    output = dnn.forward(np.transpose(np.array(inputData,dtype="float32")))
+    output = output.T
+    
+    for i in range(len(output)):
+        maxIndex = util.chooseMax(output[i])
+        out.write("{0},{1}\n".format(data[i][0],trans[maxIndex][1]))
+
+
+    #for row in data:
+        #max_index = util.chooseMax(dnn.forward(row[1:]))
+        #out.write("{0},{1}\n".format(row[0], trans[max_index][1]))
+        #print("{0}/{1}".format(cnt, len(data)))
+        #cnt += 1
 
 if __name__ == '__main__':
     main()
